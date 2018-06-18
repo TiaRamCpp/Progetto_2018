c7467f4d7df268c8cefb7c39abea246276713524
Mattia Ramella Livrin <mattia1404@gmail.com>
2018-06-18 21:55:40 +0200

    aggiunta file gestione_relazioni.h, ricerca.h, classe Nodo

---
+++ Progetto_2018/Nodo.cpp
@@ -0,0 +1,98 @@
+#include "Nodo.h"
+
+using namespace std;
+
+Nodo::Nodo(int id)
+{
+	//imposto id nodo e inizializzo vettori per relazioni e relative tipologie
+	_id = id;
+	_id_archi.clear();
+	_tipologia_relazioni.clear();
+}
+
+Nodo::~Nodo()
+{
+}
+
+void Nodo::modificaIdNodo(int id)
+{
+	_id = id;
+}
+
+bool Nodo::aggiungiRelazione(string id, int tipo_relazione)
+{
+	bool esistente = false;
+	unsigned int i = 0;
+
+	//controllo che relazione cercata sia esistente
+	for (; (i < _id_archi.size()) && (!esistente); i++)
+	{
+		if (id == _id_archi[i])
+		{
+			esistente = true;
+		}
+	}
+
+	//se non esistente lo aggiungo
+	if (!esistente)
+	{
+		_id_archi.push_back(id);
+		_tipologia_relazioni.push_back(tipo_relazione);
+	}
+
+	return esistente;
+}
+
+bool Nodo::modificaRelaizone(string id, int tipo_relazione)
+{
+	bool esistente = false;
+	unsigned int i = 0;
+
+	//controllo che relazione cercata sia esistente
+	for (; (i < _id_archi.size()) && (!esistente); i++)
+	{
+		if (id == _id_archi[i])
+		{
+			esistente = true;
+		}
+	}
+
+	//se esistente allora lo modifico
+	if (esistente) 
+	{
+		_tipologia_relazioni[i] = tipo_relazione;
+	}
+
+	return esistente;
+}
+
+bool Nodo::rimuoviRelazione(string id)
+{
+	bool esistente = false;
+	unsigned int i = 0;
+
+	//controllo che relazione sia esistente
+	for (; (i < _id_archi.size())&&(!esistente); i++) 
+	{
+		if (id == _id_archi[i]) 
+		{
+			esistente = true;
+		}
+	}
+
+	//se esistente elimino id e relativa relazione
+	if(esistente)
+	{
+		_id_archi.erase(_id_archi.begin()+i+1);
+		_tipologia_relazioni.erase(_tipologia_relazioni.begin() + i+1);
+	}
+	
+	return esistente;
+}
+
+int Nodo::numeroRelazioni()
+{
+	//numero di archi con altri nodi
+	return _id_archi.size();
+}
+
---
+++ Progetto_2018/Nodo.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#define AMICO 1
+#define CONOSCENTE 2
+#define CONIUGE 3
+#define FIGLIO 4
+#define GENITORE 5
+#define DIPENDENTE 6
+#define CONSOCIATA 7
+#define COLLABORAZIONE 8
+
+using namespace std;
+
+class Nodo
+{
+public:
+	//costruttore
+	Nodo(int id);	
+	//distruttore
+	~Nodo();	
+
+	//modifico id nodo
+	void modificaIdNodo(int id);  
+	
+	//rimuovo, modifico o aggiungo relazione del nodo con altri
+	bool aggiungiRelazione(string id,int tipo_relazione);
+	bool modificaRelaizone(string id,int tipo_relazione);
+	bool rimuoviRelazione(string id);
+
+	//grado in e out
+	int numeroRelazioni();
+
+private:
+	string _id;
+	vector<string> _id_archi;
+	vector<int> _tipologia_relazioni;
+};
+
